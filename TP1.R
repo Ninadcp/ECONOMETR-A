d017ec3b5b3d0cf21c7eb6c84d335de57c7655c3
Sebiburgermeister <sburgermeister@udesa.edu.ar>
2025-03-15 13:04:24 -0300

    2da sebi

--- TP1.R
+++ TP1.R
@@ -153,7 +153,7 @@ colnames(coef_mat) <- c("Beta_0", "Beta_1", "Beta_2", "Beta_3")
 
 for (i in 1:50)
 {
-  modelo <- lm(y ~ x1 + x2 + x3, data = df_list[[i]]) 
+  modelo <- lm(y ~ x1 + x2 + x3, data = df_list2[[i]]) 
   coef_mat[i, ] <- coef(modelo)
   
 }
@@ -184,3 +184,134 @@ ggplot(coef_df2, aes(x = Beta_1, y = Beta_3)) +
   labs(title = "Estimaciones: β1 vs β3", x = "β1", y = "β3") + #NINACAMBIÁ ESTO
   theme_minimal()
 ggsave("plot6_2.png")
+
+
+#Ejercicio 2--------------------------------
+
+
+num_datasets <- 50
+
+df_list3 <- vector("list", num_datasets)
+
+
+for (i in 1:50) {
+  set.seed(i)
+  
+  n <- 100
+  #valores verdaderos
+  b_0 <- 100
+  b_1 <- 3
+  b_2 <- 2
+  b_3 <- -1
+  
+  
+  x1 <- runif(n, min = 0, max = 200)
+  x2 <- runif(n, min = 0, max = 200)
+  x3 <- runif(n, min = 0, max = 200)
+  
+  u <- rnorm(n, mean = 0, sd = sqrt(1100))
+  
+  y <- b_0 + b_1 * x1 + b_2 * x2 + b_3 * x3 + u
+  
+  #creamos los df
+  df_list3[[i]] <- data.frame(y, x1, x2, x3, u)   
+}  
+
+head(df_list3[[1]])
+
+coef_mat3 <- matrix(nrow=50, ncol=3)
+colnames(coef_mat3) <- c("Beta_0", "Beta_1","Beta_3")  
+
+for (i in 1:50)
+{
+  modelo <- lm(y ~ x1 + x3, data = df_list[[i]]) 
+  coef_mat3[i, ] <- coef(modelo)
+  
+}
+
+df_coef3 <- as.data.frame(coef_mat3)
+# Guardar en un excel
+write.xlsx(df_coef3, "tercera_estimacion.xlsx", overwrite = TRUE)
+
+#______________________________Gráfico______________________________________
+#cargamos el archivo
+coef_df3 <- read.xlsx("tercera_estimacion.xlsx")
+head(coef_df3)
+#primer gráfico beta_1 beta_3
+
+ggplot(coef_df3, aes(x = Beta_1, y = Beta_3)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  labs(title = "Estimaciones: β1 vs β3", x = "β1", y = "β3") + #NINACAMBIÁ ESTO
+  theme_minimal()
+ggsave("plot21.png")
+
+
+#----Ejercicio 2 b) 
+
+
+
+df_list4 <- vector("list", num_datasets)
+
+
+
+for (i in 1:50) {
+  set.seed(i)
+  
+  n <- 100
+  x1 <- runif(n, min = 0, max = 200)
+  x2 <- scale(matrix( rnorm(100), ncol=1 ))
+  xs <- cbind(scale(x1),x2)
+  c1 <- var(xs)
+  chol1 <- solve(chol(c1))
+  newx <- xs
+  newc <- matrix(
+    c(1 , 0.987,
+      0.987, 1 ), ncol=2)
+  eigen(newc)
+  chol2 <- chol(newc)
+  xm2 <- newx%*% chol2 * sd(x1) + mean(x1)
+  x2 <- xm2[, 2]
+  
+  x3 <- runif(n, min = 0, max = 200)
+  
+  
+  u <- rnorm(n, mean = 0, sd = sqrt(1100))
+  
+  y <- b_0 + b_1 * x1 + b_2 * x2 + b_3 * x3 + u
+  
+  #creamos los df
+  df_list4[[i]] <- data.frame(y, x1, x2, x3, u)   
+}  
+
+head(df_list4[[1]])
+
+coef_mat4 <- matrix(nrow=50, ncol=3)
+colnames(coef_mat4) <- c("Beta_0", "Beta_1", "Beta_3")  
+
+for (i in 1:50)
+{
+  modelo <- lm(y ~ x1 + x3, data = df_list2[[i]]) 
+  coef_mat4[i, ] <- coef(modelo)
+  
+}
+
+df_coef4 <- as.data.frame(coef_mat4)
+# Guardar en un excel
+write.xlsx(df_coef4, "cuarta_estimacion.xlsx", overwrite = TRUE)
+
+##---6
+
+#cargamos el archivo
+coef_df4 <- read.xlsx("cuarta_estimacion.xlsx")
+head(coef_df4)
+
+#segundo gráfico beta_1 beta_3
+
+ggplot(coef_df4, aes(x = Beta_1, y = Beta_3)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  labs(title = "Estimaciones: β1 vs β3", x = "β1", y = "β3") + #NINACAMBIÁ ESTO
+  theme_minimal()
+ggsave("plot2ult.png")
+
