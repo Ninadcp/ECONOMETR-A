ce9324b11b9684e7a043428968289865db2536ed
Sebiburgermeister <sburgermeister@udesa.edu.ar>
2025-03-15 12:25:51 -0300

    avances sebi

--- TP1.R
+++ TP1.R
@@ -7,7 +7,11 @@ rm(list = ls())
 # Set directory
 getwd() 
 #setwd("/Users/ninadicostanzopereira/Desktop/CORE/ECON/tp1") #mi computadora
-setwd("/Users/ninadicostanzopereira/Desktop/ECONOMETR-A") #GIT
+#setwd("/Users/ninadicostanzopereira/Desktop/ECONOMETR-A") #GIT 
+setwd("C:/Users/sebib/Documents/GitHub/ECONOMETR-A")
+
+
+
 dir()
 #install.packages("xtable")
 #install.packages("openxlsx")
@@ -17,10 +21,12 @@ library(xtable)
 library(ggplot2)
 
 
-#EJERCICIO 1
+
 
 num_datasets <- 50
+
 df_list <- vector("list", num_datasets)
+
 
 for (i in 1:50) {
   set.seed(i)
@@ -47,7 +53,7 @@ for (i in 1:50) {
 
 head(df_list[[1]])
 
-##correlación
+#-----------------------------2------------------------correlación
 cor_list <- vector("list", num_datasets)
 for (i in 1:50)
 {
@@ -65,7 +71,7 @@ sink("correlaciontp1.tex")
 print(cor_table, type = "latex", include.rownames = TRUE, include.colnames= TRUE)
 sink()
 
-# Estime β0, β1, β2 y β3 para las 50 muestras y guarde los datos en un Excel llamado
+# -----------3-Estime β0, β1, β2 y β3 para las 50 muestras y guarde los datos en un Excel llamado
 #primera_estimacion.xlsx. Tiene que quedarle una planilla con cuatro columnas y
 #50 estimaciones distintas.
 
@@ -105,14 +111,31 @@ ggplot(df_coef, aes(x = Beta_1, y = Beta_3)) +
   theme_minimal()
 ggsave("plot4_2.png")
 
-#_______________________-5_________________________________
+#_______________________5_________________________________
 
-df_list5 <- vector("list", num_datasets)
+df_list2 <- vector("list", num_datasets)
+
+
 
 for (i in 1:50) {
   set.seed(i)
   
   n <- 100
+  x1 <- runif(n, min = 0, max = 200)
+  x2 <- scale(matrix( rnorm(100), ncol=1 ))
+  xs <- cbind(scale(x1),x2)
+  c1 <- var(xs)
+  chol1 <- solve(chol(c1))
+  newx <- xs
+  newc <- matrix(
+    c(1 , 0.987,
+      0.987, 1 ), ncol=2)
+  eigen(newc)
+  chol2 <- chol(newc)
+  xm2 <- newx%*% chol2 * sd(x1) + mean(x1)
+  x2 <- xm2[, 2]
+  
+  x3 <- runif(n, min = 0, max = 200)
   
   
   u <- rnorm(n, mean = 0, sd = sqrt(1100))
@@ -120,10 +143,44 @@ for (i in 1:50) {
   y <- b_0 + b_1 * x1 + b_2 * x2 + b_3 * x3 + u
   
   #creamos los df
-  df_list5[[i]] <- data.frame(y, x1, x2, x3, u)   
+  df_list2[[i]] <- data.frame(y, x1, x2, x3, u)   
 }  
 
-head(df_list[[1]])
+head(df_list2[[1]])
 
+coef_mat <- matrix(nrow=50, ncol=4)
+colnames(coef_mat) <- c("Beta_0", "Beta_1", "Beta_2", "Beta_3")  
 
+for (i in 1:50)
+{
+  modelo <- lm(y ~ x1 + x2 + x3, data = df_list[[i]]) 
+  coef_mat[i, ] <- coef(modelo)
+  
+}
 
+df_coef2 <- as.data.frame(coef_mat)
+# Guardar en un excel
+write.xlsx(df_coef2, "segunda_estimacion.xlsx", overwrite = TRUE)
+
+##---6
+
+#cargamos el archivo
+coef_df2 <- read.xlsx("segunda_estimacion.xlsx")
+head(coef_df2)
+#primer gráfico beta_1 beta_2
+
+ggplot(coef_df2, aes(x = Beta_1, y = Beta_2)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  labs(title = "Estimaciones: β1 vs β2", x = "β1", y = "β2") + #NINACAMBIÁ ESTO
+  theme_minimal()
+ggsave("plot6_1.png")
+
+#segundo gráfico beta_1 beta_3
+
+ggplot(coef_df2, aes(x = Beta_1, y = Beta_3)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", color = "red", se = FALSE) +
+  labs(title = "Estimaciones: β1 vs β3", x = "β1", y = "β3") + #NINACAMBIÁ ESTO
+  theme_minimal()
+ggsave("plot6_2.png")
