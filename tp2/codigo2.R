3a68f9ac2810b27e3d50517db11cba7935e0eeaa
cahuertaa <ca.huertaaraujo@gmail.com>
2025-04-18 22:48:44 -0300

    Update codigo2.R

--- tp2/codigo2.R
+++ tp2/codigo2.R
@@ -2,6 +2,7 @@
 #install.packages("stargazer")  # para exportar en latex fyi
 #install.packages('fixest')
 #install.packages("plm")
+#install.packages("dplyr")
 rm(list=ls())
 #librerías necesarias
 library(readr)
@@ -11,7 +12,7 @@ library(fixest)
 library(plm)
 
 #set directorio
-setwd("/Users/ninadicostanzopereira/Desktop/ECONOMETR-A/tp2/")
+setwd("/Users/CARLOS/OneDrive/Escritorio/R/TP2")
 
 #cargar datos
 df <- read_csv("cornwell.csv")
@@ -30,7 +31,7 @@ pdata <- pdata.frame(df, index = c("county", "year"))
 modelo_fe <- plm(lcrmrte ~ lprbarr + lprbconv + lprbpris + lavgsen + lpolpc + ldensity+ ltaxpc + lpctmin + lwcon + lwtuc + lwtrd + lwfir + lwser + lwmfg + lwfed + lwsta + lwloc + lmix + lpctymle , data = pdata, model = "within")
 
 summary(modelo_fe)
-stargazer(modelo, type = "latex", out = "modelofe.tex") #guarda en .txt para latex
+stargazer(modelo_fe, type = "latex", out = "modelofe.tex") #guarda en .txt para latex
 
 #-----------------------------------d------------------------------------------
 #restar medias, sacamos el promedio histórico x estado.
@@ -58,11 +59,11 @@ for (x in vars) {
 
 modelofwl <- lm(lcrmrte_n ~ lprbarr_n + lprbconv_n + lprbpris_n + lavgsen_n + lpolpc_n + ldensity_n + ltaxpc_n + lpctmin_n + lwcon_n + lwtuc_n + lwtrd_n + lwfir_n + lwser_n + lwmfg_n + lwfed_n + lwsta_n + lwloc_n + lmix_n + lpctymle_n + urban_n + central_n + west_n - 1, data = df)
 summary(modelofwl)
-stargazer(modelo, type = "latex", out = "modelofwl.tex") #guarda en .txt para latex
+stargazer(modelofwl, type = "latex", out = "modelofwl.tex") #guarda en .txt para latex
 
 #-----------------------------------e------------------------------------------
 modelofwl2 <- lm(lcrmrte ~ lprbarr_n + lprbconv_n + lprbpris_n + lavgsen_n + lpolpc_n + ldensity_n + ltaxpc_n + lpctmin_n + lwcon_n + lwtuc_n + lwtrd_n + lwfir_n + lwser_n + lwmfg_n + lwfed_n + lwsta_n + lwloc_n + lmix_n + lpctymle_n + urban_n + central_n + west_n - 1, data = df)
 summary(modelofwl2)
-stargazer(modelo, type = "latex", out = "modelofwl2.tex") #guarda en .txt para latex
+stargazer(modelofwl2, type = "latex", out = "modelofwl2.tex") #guarda en .txt para latex
 
 
